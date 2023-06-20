0566309e1deb731fca0de4979a2ef9530216b451
Aaryan Bhaaii <20uec035@lnmiit.ac.in>
2023-06-21 00:10:41 +0530

    second

--- app.js
+++ app.js
@@ -312,6 +312,6 @@ app.post("/register", function(req, res) {
 });
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
 });
